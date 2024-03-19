ab64da5fdb70cf07305aa42ddb9ac26ece3233aa
muhayimanatheo <muhayitheo23@gmail.com>
2024-03-19 14:06:14 +0200

    table created

--- buffer_app/models.py
+++ buffer_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Publication(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='publications/')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
